f01a482a8c6318222a20ffd9818414312b9e1dc4
wasya1212 <leonardo.1212@yandex.ua>
2018-12-05 00:35:50 +0200

    ready for production

---
+++ build/precache-manifest.a377f8e9d9ce2bde4aa50a1339e1a19a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "46e3b8575a49e92ef03fb0872cd2c149",
+    "url": "/static/media/full-charge.46e3b857.png"
+  },
+  {
+    "revision": "083e81c4562743def5c5eb7de5c8be20",
+    "url": "/static/media/empty-charge.083e81c4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39d95b47ef8b411aad76",
+    "url": "/static/js/main.39d95b47.chunk.js"
+  },
+  {
+    "revision": "ed05a541ae0689ed5229",
+    "url": "/static/js/1.ed05a541.chunk.js"
+  },
+  {
+    "revision": "39d95b47ef8b411aad76",
+    "url": "/static/css/main.7146c986.chunk.css"
+  },
+  {
+    "revision": "836ebf045172ecc76b142947e8c4eaf7",
+    "url": "/index.html"
+  }
+];
